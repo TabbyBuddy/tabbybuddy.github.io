e8aa5eb04e21388e6707b2304cf5e8c1f4db6bb8
minwoool <43971931+minwoool@users.noreply.github.com>
2020-10-17 18:30:55 +0900

    Update dccon_list.js

--- dcc_chat/lib/dccon_list.js
+++ dcc_chat/lib/dccon_list.js
@@ -337,6 +337,6 @@ dcConsData = [
 	{name:"00247.gif",			keywords:["회전띵킹1"],			tags:["리사수콘"]},
 	{name:"00248.gif",			keywords:["회전띵킹2"],			tags:["리사수콘"]},
 	
-	{name:"00249.png",			keywords:["복잡띵킹"],			tags:["리사수콘"]}
+	{name:"00249.gif",			keywords:["복잡띵킹"],			tags:["리사수콘"]}
 	
 ];
