d0939df331f2d79225988dce06d6bf766e9d1d7c
tabbyx2 <max886@naver.com>
2023-06-24 02:41:56 +0900

    230624
    230624

---
+++ dcc_chat_art/lib/config.js
@@ -0,0 +1,36 @@
+configData = {
+	numChatMax				: 20,
+	personalColor			:	/*false*/true,
+	badgeVisible			: false,
+	themeURI					: "",
+	theme							: "default",
+	themeName					: "",
+	msgExistDuration	: 0,
+	msgAniDuration		: 0,
+	debugLevel				:	2,
+	useDisplayName		: true,
+	loadCheerImgs			: true,
+	loadTwitchCons		: true,
+	consRealSubsOnly	: true,
+	loadDcCons				: true,
+	dcConsURI					: "",
+  subMonthsMsg			: "{!0:{months} 개월 }구독 {0: 시작}!!",
+  cheersMsg         : "{!0:{bits} 비트 }를 후원!!",
+	loadClipPreview		: true,
+	clipReplaceMsg		: "[ 클립 ]",
+	webSocket					: "wss://irc-ws.chat.twitch.tv:443",
+	nick							: "justinfan16831",
+	pass							: "foobar",
+	channel						: "#tabby_x2",
+	retryInterval			: 2,
+	allMessageHandle	: /*false*/false,
+	muteUser					: ["Nightbot", "Ssakdook", "twipkr"],
+	deleteBanMsg			: true,
+	commands					: [
+		{exe:"clear", msg:"!!clear"},
+		{exe:"theme", msg:"!!theme"}
+	],
+	replaceMsgs				: [
+		{orig:/^![a-zA-Z]+/, to:"{no_display}"}			// 봇 호출 영문 메세지 미표시
+	]
+};
---
+++ dcc_chat_art/lib/dccon_list.js
@@ -0,0 +1,338 @@
+dcConsData = [
+
+//슬픈냥이콘
+	{name:"00001.gif",			keywords:["으앙냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00002.gif",			keywords:["슬픈냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00003.gif",			keywords:["엿냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00004.gif",			keywords:["믿음냥이"],			tags:["슬픈냥이콘"]},		
+
+	{name:"00005.gif",			keywords:["눙물냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00006.gif",			keywords:["와꾸냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00007.gif",			keywords:["잘자냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00008.gif",			keywords:["배고픈냥이"],		tags:["슬픈냥이콘"]},		
+
+	{name:"00009.gif",			keywords:["눈물냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00010.gif",			keywords:["모자냥이"],		tags:["슬픈냥이콘"]},		
+	{name:"00011.gif",			keywords:["저격냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00012.gif",			keywords:["으아앙냥이"],			tags:["슬픈냥이콘"]},
+	
+	{name:"00013.gif",			keywords:["토냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00014.gif",			keywords:["눈물냥이2"],			tags:["슬픈냥이콘"]},		
+	{name:"00015.gif",			keywords:["먹고픈냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00016.gif",			keywords:["쓸쓸냥이"],			tags:["슬픈냥이콘"]},		
+
+	{name:"00017.gif",			keywords:["와꾸냥이2"],			tags:["슬픈냥이콘"]},		
+	{name:"00018.gif",			keywords:["멍청냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00019.gif",			keywords:["눈물냥이3"],			tags:["슬픈냥이콘"]},		
+	{name:"00020.gif",			keywords:["시가냥이"],			tags:["슬픈냥이콘"]},		
+
+	{name:"00021.gif",			keywords:["와꾸냥이3"],			tags:["슬픈냥이콘"]},		
+	{name:"00022.gif",			keywords:["슬픈냥이2"],			tags:["슬픈냥이콘"]},		
+	{name:"00023.gif",			keywords:["데부냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00024.gif",			keywords:["와꾸냥이4"],			tags:["슬픈냥이콘"]},		
+
+	{name:"00025.gif",			keywords:["으악냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00026.png",			keywords:["충격냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00027.gif",			keywords:["흐음냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00028.gif",			keywords:["증발냥이"],			tags:["슬픈냥이콘"]},		
+
+	{name:"00029.gif",			keywords:["ㅈㅅ냥이"],			tags:["슬픈냥이콘"]},		
+	{name:"00030.gif",			keywords:["흉기냥이"],			tags:["슬픈냥이콘"]},		
+//찌그러진 개구리콘
+	{name:"00031.png",			keywords:["예스개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00032.png",			keywords:["십자개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00033.png",			keywords:["후훗개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00034.png",			keywords:["꿀잼개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00035.png",			keywords:["졸잼개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00036.png",			keywords:["광대개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00037.png",			keywords:["우씨개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00038.png",			keywords:["뻐큐개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00039.png",			keywords:["히키개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00040.png",			keywords:["팬티개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00041.png",			keywords:["좋은개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00042.png",			keywords:["노개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00043.png",			keywords:["흐음개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00044.png",			keywords:["헤헤개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00045.png",			keywords:["취침개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00046.gif",			keywords:["청취개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00047.gif",			keywords:["발광개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00048.gif",			keywords:["박수개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00049.gif",			keywords:["ㅄ개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00050.gif",			keywords:["댄스개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00051.gif",			keywords:["패는개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00052.png",			keywords:["내눈개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00053.png",			keywords:["무룩개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00054.png",			keywords:["행복개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00055.png",			keywords:["히죽개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00056.png",			keywords:["어이개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00057.png",			keywords:["화난개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00058.png",			keywords:["권총개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00059.png",			keywords:["힙한개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00060.png",			keywords:["닌자개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00061.png",			keywords:["우울개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00062.png",			keywords:["임마개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00063.png",			keywords:["도트개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00064.png",			keywords:["와개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00065.png",			keywords:["으악개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00066.png",			keywords:["증발개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00067.png",			keywords:["지적개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00068.png",			keywords:["에잉개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00069.png",			keywords:["눈물개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00070.png",			keywords:["불면개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00071.png",			keywords:["슬픈개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00072.png",			keywords:["허허개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00073.png",			keywords:["핥짝개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00074.png",			keywords:["레알개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00075.png",			keywords:["포기개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00076.png",			keywords:["힙개구리"],			tags:["찌그러진개구리콘"]},		
+
+	{name:"00077.png",			keywords:["대박개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00078.png",			keywords:["맥주개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00079.png",			keywords:["돋보개구리"],			tags:["찌그러진개구리콘"]},		
+	{name:"00080.png",			keywords:["ㅅㅂ개구리"],			tags:["찌그러진개구리콘"]},			
+
+	{name:"00193.gif",			keywords:["깜놀개구리"],			tags:["찌그러진개구리콘"]},
+
+
+	//야인시대콘(심영콘)
+	{name:"00081.gif",			keywords:["야이정신병자야"],			tags:["심영"]},		
+	{name:"00082.gif",			keywords:["봐줘ㅅㅇ"],			tags:["심영"]},		
+	{name:"00083.gif",			keywords:["단비꺼ㅅㅇ"],			tags:["심영"]},		
+	{name:"00084.gif",			keywords:["에엑따ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00085.gif",			keywords:["발광ㅅㅇ"],			tags:["심영"]},		
+	{name:"00086.gif",			keywords:["개놀람ㅅㅇ"],			tags:["심영"]},		
+	{name:"00087.gif",			keywords:["맴매ㅅㅇ"],			tags:["심영"]},		
+	{name:"00088.gif",			keywords:["맴맴매ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00089.gif",			keywords:["속구경ㅅㅇ"],			tags:["심영"]},		
+	{name:"00090.gif",			keywords:["또구경ㅅㅇ"],			tags:["심영"]},		
+	{name:"00091.gif",			keywords:["링겔ㅅㅇ"],			tags:["심영"]},		
+	{name:"00092.gif",			keywords:["ㅈㅅㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00093.gif",			keywords:["환대ㅅㅇ"],			tags:["심영"]},		
+	{name:"00094.gif",			keywords:["나죽네ㅅㅇ"],			tags:["심영"]},		
+	{name:"00095.gif",			keywords:["탈주ㅅㅇ"],			tags:["심영"]},		
+	{name:"00096.gif",			keywords:["뭐야ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00097.gif",			keywords:["웃음ㅅㅇ"],			tags:["심영"]},		
+	{name:"00098.gif",			keywords:["박수ㅅㅇ"],			tags:["심영"]},		
+	{name:"00099.gif",			keywords:["박수ㅅㅇ2"],			tags:["심영"]},		
+	{name:"00100.gif",			keywords:["탈주ㅅㅇ2"],			tags:["심영"]},		
+
+	{name:"00101.gif",			keywords:["병원ㅅㅇ"],			tags:["심영"]},		
+	{name:"00102.gif",			keywords:["뿌듯ㅅㅇ"],			tags:["심영"]},		
+	{name:"00103.gif",			keywords:["봐줘요ㅅㅇ"],			tags:["심영"]},		
+	{name:"00104.gif",			keywords:["혼미ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00105.gif",			keywords:["어무니ㅅㅇ"],			tags:["심영"]},		
+	{name:"00106.gif",			keywords:["열변ㅅㅇ"],			tags:["심영"]},		
+	{name:"00107.gif",			keywords:["웅변ㅅㅇ"],			tags:["심영"]},		
+	{name:"00108.gif",			keywords:["의사ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00109.gif",			keywords:["진찰ㅅㅇ"],			tags:["심영"]},		
+	{name:"00110.gif",			keywords:["깔깔ㅅㅇ"],			tags:["심영"]},		
+	{name:"00111.gif",			keywords:["폭발ㅅㅇ"],			tags:["심영"]},		
+	{name:"00112.gif",			keywords:["계약ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00113.gif",			keywords:["조준ㅅㅇ"],			tags:["심영"]},		
+	{name:"00114.gif",			keywords:["흐음ㅅㅇ"],			tags:["심영"]},		
+	{name:"00115.gif",			keywords:["아씨뭐하는거야"],			tags:["심영"]},		
+	{name:"00116.gif",			keywords:["빵야ㅅㅇ"],			tags:["심영"]},		
+
+	{name:"00117.gif",			keywords:["연발ㅅㅇ"],			tags:["심영"]},		
+	{name:"00118.gif",			keywords:["야바위ㅅㅇ"],			tags:["심영"]},		
+	{name:"00119.gif",			keywords:["대찬사ㅅㅇ"],			tags:["심영"]},		
+	{name:"00120.gif",			keywords:["응원ㅅㅇ"],			tags:["심영"]},
+
+
+	//댕댕콘
+	{name:"00121.gif",			keywords:["댕댕간지"],			tags:["댕댕"]},	
+	{name:"00122.png",			keywords:["댕댕경멸"],			tags:["댕댕"]},	
+	{name:"00123.png",			keywords:["댕댕근엄"],			tags:["댕댕"]},	
+	{name:"00124.png",			keywords:["댕댕깜놀"],			tags:["댕댕"]},	
+
+	{name:"00125.png",			keywords:["댕댕깜짝"],			tags:["댕댕"]},	
+	{name:"00126.png",			keywords:["댕댕깜찍"],			tags:["댕댕"]},	
+	{name:"00127.gif",			keywords:["댕댕끄덕"],			tags:["댕댕"]},	
+	{name:"00128.png",			keywords:["댕댕낼름"],			tags:["댕댕"]},	
+	
+	{name:"00129.gif",			keywords:["댕댕눈뜸"],			tags:["댕댕"]},	
+	{name:"00130.png",			keywords:["댕댕느끼"],			tags:["댕댕"]},	
+	{name:"00131.png",			keywords:["댕화들짝"],			tags:["댕댕"]},	
+	{name:"00132.png",			keywords:["댕댕똘끼"],			tags:["댕댕"]},	
+
+	{name:"00133.gif",			keywords:["댕댕띠용"],			tags:["댕댕"]},	
+	{name:"00134.png",			keywords:["댕댕모찌"],			tags:["댕댕"]},	
+	{name:"00135.png",			keywords:["댕댕모찌2"],			tags:["댕댕"]},	
+	{name:"00136.gif",			keywords:["댕댕미소"],			tags:["댕댕"]},	
+
+	{name:"00137.png",			keywords:["댕댕미친"],			tags:["댕댕"]},	
+	{name:"00138.png",			keywords:["댕댕방긋"],			tags:["댕댕"]},	
+	{name:"00139.png",			keywords:["댕댕부담"],			tags:["댕댕"]},	
+	{name:"00140.gif",			keywords:["댕댕부들"],			tags:["댕댕"]},	
+
+	{name:"00141.png",			keywords:["댕댕불쌍"],			tags:["댕댕"]},	
+	{name:"00142.png",			keywords:["댕댕빡침"],			tags:["댕댕"]},	
+	{name:"00143.png",			keywords:["댕댕빡침2"],			tags:["댕댕"]},	
+	{name:"00144.gif",			keywords:["댕댕볼살"],			tags:["댕댕"]},	
+
+	{name:"00145.png",			keywords:["댕댕슬픔"],			tags:["댕댕"]},	
+	{name:"00146.gif",			keywords:["댕댕신나"],			tags:["댕댕"]},	
+	{name:"00147.png",			keywords:["댕댕아련"],			tags:["댕댕"]},	
+	{name:"00148.png",			keywords:["댕댕어이"],			tags:["댕댕"]},	
+
+	{name:"00149.gif",			keywords:["댕댕억울"],			tags:["댕댕"]},	
+	{name:"00150.png",			keywords:["댕댕엄격"],			tags:["댕댕"]},	
+	{name:"00151.png",			keywords:["댕댕업신"],			tags:["댕댕"]},	
+	{name:"00152.gif",			keywords:["댕댕갸웃"],			tags:["댕댕"]},	
+
+	{name:"00153.gif",			keywords:["댕댕웃음"],			tags:["댕댕"]},	
+	{name:"00154.png",			keywords:["댕댕윙크"],			tags:["댕댕"]},	
+	{name:"00155.png",			keywords:["댕댕정색"],			tags:["댕댕"]},	
+	{name:"00156.png",			keywords:["댕댕간지2"],			tags:["댕댕"]},	
+
+	{name:"00157.png",			keywords:["댕댕쪽팔"],			tags:["댕댕"]},	
+	{name:"00158.gif",			keywords:["댕댕창피"],			tags:["댕댕"]},	
+	{name:"00159.gif",			keywords:["댕댕충격"],			tags:["댕댕"]},	
+	{name:"00160.png",			keywords:["댕댕탄식"],			tags:["댕댕"]},	
+
+	{name:"00161.gif",			keywords:["댕댕폭소"],			tags:["댕댕"]},	
+	{name:"00162.gif",			keywords:["댕댕행복"],			tags:["댕댕"]},	
+	{name:"00163.gif",			keywords:["댕댕확대"],			tags:["댕댕"]},	
+	{name:"00164.png",			keywords:["댕댕황당"],			tags:["댕댕"]},	
+
+	{name:"00165.gif",			keywords:["댕댕갸웃2"],			tags:["댕댕"]},	
+	{name:"00166.png",			keywords:["댕댕정색2"],			tags:["댕댕"]},	
+	{name:"00167.gif",			keywords:["댕못마땅"],			tags:["댕댕"]},	
+	{name:"00168.gif",			keywords:["댕뚝배기"],			tags:["댕댕"]},	
+	
+	{name:"00169.gif",			keywords:["댕댕크앙"],			tags:["댕댕"]},	
+	{name:"00170.gif",			keywords:["댕댕핥핥"],			tags:["댕댕"]},	
+	
+
+	//껍질미리깐달걀콘
+	{name:"00171.png",			keywords:["즐겁다"],			tags:["깐달걀콘"]},	
+	{name:"00172.png",			keywords:["혐오한다"],			tags:["깐달걀콘"]},	
+	{name:"00173.png",			keywords:["실망하다"],			tags:["깐달걀콘"]},	
+	{name:"00174.png",			keywords:["마참내"],			tags:["깐달걀콘"]},
+
+	{name:"00175.png",			keywords:["가즈아"],			tags:["깐달걀콘"]},	
+	{name:"00176.png",			keywords:["두렵다"],			tags:["깐달걀콘"]},	
+	{name:"00177.png",			keywords:["띠껍다"],			tags:["깐달걀콘"]},
+
+
+	//나인우콘
+	{name:"00178.gif",			keywords:["지금갑니다"],			tags:["나인우콘"]},	
+	{name:"00179.gif",			keywords:["끄덕끄덕"],			tags:["나인우콘"]},	
+	{name:"00180.gif",			keywords:["도리도리"],			tags:["나인우콘"]},	
+	{name:"00181.gif",			keywords:["지긋이"],			tags:["나인우콘"]},
+
+	{name:"00182.gif",			keywords:["신난당"],			tags:["나인우콘"]},	
+	{name:"00183.gif",			keywords:["눈깔굴림"],			tags:["나인우콘"]},	
+	{name:"00184.gif",			keywords:["눈깔굴림2"],			tags:["나인우콘"]},
+	{name:"00185.gif",			keywords:["안녕"],			tags:["나인우콘"]},	
+
+	{name:"00186.gif",			keywords:["긁적"],			tags:["나인우콘"]},	
+	{name:"00187.gif",			keywords:["와"],			tags:["나인우콘"]},	
+	{name:"00188.gif",			keywords:["포기"],			tags:["나인우콘"]},
+	{name:"00189.gif",			keywords:["눙물"],			tags:["나인우콘"]},	
+
+	{name:"00190.gif",			keywords:["좋카죽카"],			tags:["나인우콘"]},	
+	{name:"00191.gif",			keywords:["끄덕"],			tags:["나인우콘"]},	
+	{name:"00192.gif",			keywords:["끄덕덕"],			tags:["나인우콘"]},	
+			
+	
+	//톰과제리콘
+	{name:"00194.gif",			keywords:["제리흔들"],			tags:["톰과제리콘"]},	
+	{name:"00195.gif",			keywords:["제리배꼽웃음"],			tags:["톰과제리콘"]},	
+	{name:"00196.gif",			keywords:["헬스제리"],			tags:["톰과제리콘"]},	
+	{name:"00197.gif",			keywords:["제리기도"],			tags:["톰과제리콘"]},
+
+	{name:"00198.gif",			keywords:["제리빼꼼"],			tags:["톰과제리콘"]},	
+	{name:"00199.gif",			keywords:["제리약탈"],			tags:["톰과제리콘"]},	
+	{name:"00200.gif",			keywords:["제리꿀잠"],			tags:["톰과제리콘"]},
+	{name:"00201.gif",			keywords:["제리취침"],			tags:["톰과제리콘"]},	
+
+	{name:"00202.gif",			keywords:["제리끄덕"],			tags:["톰과제리콘"]},	
+	{name:"00203.gif",			keywords:["제리손놀이"],			tags:["톰과제리콘"]},	
+	{name:"00204.gif",			keywords:["제리두근"],			tags:["톰과제리콘"]},
+	{name:"00205.gif",			keywords:["제리웃음"],			tags:["톰과제리콘"]},	
+
+	{name:"00206.gif",			keywords:["제리이별"],			tags:["톰과제리콘"]},	
+	{name:"00207.gif",			keywords:["제리안녕"],			tags:["톰과제리콘"]},	
+	{name:"00208.gif",			keywords:["제리뚝배기"],			tags:["톰과제리콘"]},
+	{name:"00209.gif",			keywords:["제리복통"],			tags:["톰과제리콘"]},
+	
+	{name:"00210.gif",			keywords:["제리인사"],			tags:["톰과제리콘"]},	
+	{name:"00211.gif",			keywords:["제리홀짝"],			tags:["톰과제리콘"]},	
+	{name:"00212.gif",			keywords:["맹수제리"],			tags:["톰과제리콘"]},
+	{name:"00213.gif",			keywords:["코스프레"],			tags:["톰과제리콘"]},
+	
+	{name:"00214.gif",			keywords:["제리컴온"],			tags:["톰과제리콘"]},	
+	{name:"00215.gif",			keywords:["입술도둑"],			tags:["톰과제리콘"]},	
+	{name:"00216.gif",			keywords:["쉐도복싱"],			tags:["톰과제리콘"]},
+	{name:"00217.gif",			keywords:["제리발싸"],			tags:["톰과제리콘"]},
+	
+	{name:"00218.gif",			keywords:["기어서드"],			tags:["톰과제리콘"]},	
+	{name:"00219.gif",			keywords:["오버파워"],			tags:["톰과제리콘"]},	
+	{name:"00220.gif",			keywords:["제리부빗"],			tags:["톰과제리콘"]},
+	{name:"00221.gif",			keywords:["제리쳐맞"],			tags:["톰과제리콘"]},
+	
+	{name:"00222.gif",			keywords:["제리눈비빔"],			tags:["톰과제리콘"]},	
+	{name:"00223.gif",			keywords:["제리들킴"],			tags:["톰과제리콘"]},	
+	{name:"00224.gif",			keywords:["냄시나요"],			tags:["톰과제리콘"]},
+	{name:"00225.gif",			keywords:["끼제리"],			tags:["톰과제리콘"]},
+	
+	{name:"00226.gif",			keywords:["끼제리2"],			tags:["톰과제리콘"]},	
+	{name:"00227.gif",			keywords:["제리쥐포"],			tags:["톰과제리콘"]},	
+	{name:"00228.gif",			keywords:["톰회축"],			tags:["톰과제리콘"]},
+	{name:"00229.gif",			keywords:["회피만렙"],			tags:["톰과제리콘"]},
+	
+	{name:"00230.gif",			keywords:["교통사고"],			tags:["톰과제리콘"]},	
+	{name:"00231.gif",			keywords:["제리인사2"],			tags:["톰과제리콘"]},	
+	{name:"00232.gif",			keywords:["각제리"],			tags:["톰과제리콘"]},
+	{name:"00233.gif",			keywords:["지휘톰"],			tags:["톰과제리콘"]},
+	
+	{name:"00234.gif",			keywords:["톰인사"],			tags:["톰과제리콘"]},	
+	{name:"00235.gif",			keywords:["화해의춤"],			tags:["톰과제리콘"]},	
+	
+	
+	
+	//리사수콘
+	{name:"00236.png",			keywords:["암드"],			tags:["리사수콘"]},	
+	{name:"00237.png",			keywords:["라이젠"],			tags:["리사수콘"]},	
+	{name:"00238.png",			keywords:["웰컴암드"],			tags:["리사수콘"]},
+	{name:"00239.png",			keywords:["계획대로수"],			tags:["리사수콘"]},
+	
+	{name:"00240.png",			keywords:["고우시다"],			tags:["리사수콘"]},	
+	{name:"00241.png",			keywords:["암풍당당"],			tags:["리사수콘"]},
+	
+	
+	
+	//관짝댄스콘
+	{name:"00242.gif",			keywords:["관짝준비"],			tags:["관짝댄스콘"]},	
+	{name:"00243.gif",			keywords:["관짝댄스"],			tags:["관짝댄스콘"]},	
+	{name:"00244.gif",			keywords:["관짝댄스2"],			tags:["관짝댄스콘"]},
+	
+	
+	//띵킹페이스콘
+	{name:"00245.png",			keywords:["띵킹1"],			tags:["리사수콘"]},	
+	{name:"00246.png",			keywords:["띵킹2"],			tags:["리사수콘"]},	
+	{name:"00247.gif",			keywords:["회전띵킹1"],			tags:["리사수콘"]},
+	{name:"00248.gif",			keywords:["회전띵킹2"],			tags:["리사수콘"]},
+	
+	{name:"00249.gif",			keywords:["복잡띵킹"],			tags:["리사수콘"]}
+	
+];
